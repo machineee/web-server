c48d2e0ec79e7ebc52c8b7f1804af451fcce9a4e
Boban <bobadilla988@gmail.com>
2015-11-19 22:21:59 +0100

    Updated language

--- server.js
+++ server.js
@@ -2,17 +2,7 @@ var express = require('express');
 var app = express();
 var PORT= 3000;
 
-var middleware = {
-	requireAuthentification: function (req, res, next) {
-		console.log('private route hit');
-		next();
-	}, 
-	logger: function (req, res, next) {
-		var date = new Date();
-		console.log('Request: ' +  ' '+ date.toString() + ' ' + req.method + ' ' + req.originalUrl);
-		next();
-	}
-};
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 
